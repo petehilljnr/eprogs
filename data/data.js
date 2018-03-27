2a178d5fdc26d45c4d1f276e036634253bb0e7b3
Hill <Peter.Hill@stantec.com>
2018-03-27 14:39:09 +1300

    update photos

--- data/data.js
+++ data/data.js
@@ -7,7 +7,7 @@ var home_team = {
 			"name" : "Matt Jones",
 			"photo" : "photos/matt_jones.png",
 			"birth_date" : "",
-			"high_school" : "",
+			"prev_club" : "",
 			"fav_food" : "",
 			"occupation" : ""
 		},
@@ -15,7 +15,124 @@ var home_team = {
 			"position" : "14",
 			"name" : "Jordan McEntee",
 			"photo" : "photos/jordan_mcentee.png",
-			"birth_date" : "",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "13",
+			"name" : "Logan Moore",
+			"photo" : "photos/logan_moore.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "12",
+			"name" : "Danny Van Der Voort",
+			"photo" : "photos/Danny_Van_Der_Voort.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "11",
+			"name" : "Chris Whitley",
+			"photo" : "photos/christopher_whitley.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "10",
+			"name" : "Ben Miller",
+			"photo" : "photos/ben_miller.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "9",
+			"name" : "Kadin Brocks",
+			"photo" : "photos/kadin_brocks.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "8",
+			"name" : "Shane Calder",
+			"photo" : "photos/shane_calder.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "7",
+			"name" : "Slade McDowall",
+			"photo" : "photos/slade_mcdowall.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "6",
+			"name" : "Johnny Appleby",
+			"photo" : "photos/johnny_appleby.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "5",
+			"name" : "Pita Sinamoni",
+			"photo" : "photos/pita_sinamoni.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "4",
+			"name" : "Willie Miller",
+			"photo" : "photos/willie_miller.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "3",
+			"name" : "Sidney Fidow",
+			"photo" : "photos/sidney_fidow.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "2",
+			"name" : "Lachlan Landels",
+			"photo" : "photos/lachlan_landels.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "1",
+			"name" : "Jonah Aoina (c)",
+			"photo" : "photos/jonah_aoina.png",
+			"prev_club" : "",
 			"high_school" : "",
 			"fav_food" : "",
 			"occupation" : ""
@@ -29,7 +146,120 @@ var away_team = {
 	"team" : [
 		{
 			"position" : "15",
-			"name" : "Fletcher Smith"
+			"name" : "Fletcher Smith",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "14",
+			"name" : "John Timu",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "13",
+			"name" : "John Leslie",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "12",
+			"name" : "Marc Ellis",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "11",
+			"name" : "Gavin Stark",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "10",
+			"name" : "Stephen Fennemor",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "9",
+			"name" : "Fletcher Smith",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "8",
+			"name" : "Aaran Pene",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "7",
+			"name" : "Dillon Hunt",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "6",
+			"name" : "Sione Teu",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "5",
+			"name" : "Josh Dickson",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "4",
+			"name" : "John Blackie",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "3",
+			"name" : "",
+			"photo" : "photos/unknown.png",
+			"prev_club" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
 		}
 	]
 }
