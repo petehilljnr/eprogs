c9d20fa0f077b4186c2a9bf56d4b16e7c4332892
Hill <Peter.Hill@stantec.com>
2018-03-27 13:14:58 +1300

    initial commit

---
+++ data/data.js
@@ -0,0 +1,45 @@
+var home_team = {
+	"name" : "Kaikorai",
+	"logo_path" : "img/clubs/kaikorai.png",
+	"team" : [
+		{
+			"position" : "15",
+			"name" : "Matt Jones",
+			"photo" : "photos/matt_jones.png",
+			"birth_date" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		},
+		{
+			"position" : "14",
+			"name" : "Jordan McEntee",
+			"photo" : "photos/jordan_mcentee.png",
+			"birth_date" : "",
+			"high_school" : "",
+			"fav_food" : "",
+			"occupation" : ""
+		}
+	]
+}
+
+var away_team = {
+	"name" : "University A",
+	"logo_path" : "img/clubs/university.png",
+	"team" : [
+		{
+			"position" : "15",
+			"name" : "Fletcher Smith"
+		}
+	]
+}
+
+var game_details = {
+	"location" : "Forsyth Barr Stadium",
+	"kickoff" : "7pm",
+	"trophy" : "",
+	"trophy_details" : "",
+	"referee" : "Jono Bredin",
+	"assistants" : ["",""]
+
+}
